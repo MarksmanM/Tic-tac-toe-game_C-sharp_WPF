e0e5fdbaeb1ffb6227f0362f9b2478f1dc91122f
Денис <@LAPTOP-UI3SRNJE>
2024-01-03 23:30:45 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Tic_tac_toe_game.Model;
+
+namespace Tic_tac_toe_game
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public Board Game = new Board();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            A1.Click += Button_A1_Click;
+            A2.Click += Button_A2_Click;
+            A3.Click += Button_A3_Click;
+            B1.Click += Button_B1_Click;
+            B2.Click += Button_B2_Click;
+            B3.Click += Button_B3_Click;
+            C1.Click += Button_C1_Click;
+            C2.Click += Button_C2_Click;
+            C3.Click += Button_C3_Click;
+        }
+
+
+        private void Button_A1_Click(object sender, RoutedEventArgs e)
+        {
+            if (Game.A1 == 0) //если поле незанято, то занять его, вывести нужный png на первый план, если занято, то ничего не делать
+            {
+                if (Game.Playerturn)
+                {
+                    Game.A1 = 1;
+                    A1.Background = new ImageBrush(new BitmapImage(new Uri("tic.png", UriKind.Relative)));
+                }
+                else 
+                {
+                    Game.A1 = 2;
+                    A1.Background = new ImageBrush(new BitmapImage(new Uri("tac.png", UriKind.Relative)));
+                }
+                EndGame(Game.IsSomeoneWin());
+
+            }
+        }
+
+        private void Button_A2_Click(object sender, RoutedEventArgs e)
+        {
+            if (Game.A2 == 0)
+            {
+                if (Game.Playerturn)
+                {
+                    Game.A2 = 1;
+                    A2.Background = new ImageBrush(new BitmapImage(new Uri("tic.png", UriKind.Relative)));
+                }
+                else
+                {
+                    Game.A2 = 2;
+                    A2.Background = new ImageBrush(new BitmapImage(new Uri("tac.png", UriKind.Relative)));
+                }
+                EndGame(Game.IsSomeoneWin());
+
+            }
+        }
+
+        private void Button_A3_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.Game.A3 == 0)
+            {
+                if (Game.Playerturn)
+                {
+                    Game.A3 = 1;
+                    A3.Background = new ImageBrush(new BitmapImage(new Uri("tic.png", UriKind.Relative)));
+                }
+                else
+                {
+                    Game.A3 = 2;
+                    A3.Background = new ImageBrush(new BitmapImage(new Uri("tac.png", UriKind.Relative)));
+                }
+                EndGame(Game.IsSomeoneWin());
+
+            }
+        }
+
+        private void Button_B1_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.Game.B1 == 0)
+            {
+                if (Game.Playerturn)
+                {
+                    Game.B1 = 1;
+                    B1.Background = new ImageBrush(new BitmapImage(new Uri("tic.png", UriKind.Relative)));
+                }
+                else
+                {
+                    Game.B1 = 2;
+                    B1.Background = new ImageBrush(new BitmapImage(new Uri("tac.png", UriKind.Relative)));
+                }
+                EndGame(Game.IsSomeoneWin());
+
+            }
+        }
+
+        private void Button_B2_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.Game.B2 == 0)
+            {
+                if (Game.Playerturn)
+                {
+                    Game.B2 = 1;
+                    B2.Background = new ImageBrush(new BitmapImage(new Uri("tic.png", UriKind.Relative)));
+                }
+                else
+                {
+                    Game.B2 = 2;
+                    B2.Background = new ImageBrush(new BitmapImage(new Uri("tac.png", UriKind.Relative)));
+                }
+                EndGame(Game.IsSomeoneWin());
+
+            }
+        }
+        private void Button_B3_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.Game.B3 == 0)
+            {
+                if (Game.Playerturn)
+                {
+                    Game.B3 = 1;
+                    B3.Background = new ImageBrush(new BitmapImage(new Uri("tic.png", UriKind.Relative)));
+                }
+                else
+                {
+                    Game.B3 = 2;
+                    B3.Background = new ImageBrush(new BitmapImage(new Uri("tac.png", UriKind.Relative)));
+                }
+                EndGame(Game.IsSomeoneWin());
+
+            }
+        }
+        private void Button_C1_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.Game.C1 == 0)
+            {
+                if (Game.Playerturn)
+                {
+                    Game.C1 = 1;
+                    C1.Background = new ImageBrush(new BitmapImage(new Uri("tic.png", UriKind.Relative)));
+                }
+                else
+                {
+                    Game.C1 = 2;
+                    C1.Background = new ImageBrush(new BitmapImage(new Uri("tac.png", UriKind.Relative)));
+                }
+                EndGame(Game.IsSomeoneWin());
+
+            }
+        }
+
+        private void Button_C2_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.Game.C2 == 0)
+            {
+                if (Game.Playerturn)
+                {
+                    Game.C2 = 1;
+                    C2.Background = new ImageBrush(new BitmapImage(new Uri("tic.png", UriKind.Relative)));
+                }
+                else
+                {
+                    Game.C2 = 2;
+                    C2.Background = new ImageBrush(new BitmapImage(new Uri("tac.png", UriKind.Relative)));
+                }
+                EndGame(Game.IsSomeoneWin());
+
+            }
+        }
+        private void Button_C3_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.Game.C3 == 0)
+            {
+                if (Game.Playerturn)
+                {
+                    Game.C3 = 1;
+                    C3.Background = new ImageBrush(new BitmapImage(new Uri("tic.png", UriKind.Relative)));
+                }
+                else
+                {
+                    Game.C3 = 2;
+                    C3.Background = new ImageBrush(new BitmapImage(new Uri("tac.png", UriKind.Relative)));
+                }
+                EndGame(Game.IsSomeoneWin());
+
+            }
+        }
+
+        private void EndGame(int Winner)
+        {
+            if (Winner == 0)
+            {
+
+            }
+            else if (Winner == 1)
+            {
+                MessageBox.Show("Победа Крестиков!");
+                RestartWindow();
+            }
+            else if (Winner == 2)
+            {
+                MessageBox.Show("Победа Ноликов!");
+                RestartWindow();
+            }
+        }
+
+
+
+        private void RestartWindow() 
+        {
+            A1.Background = null;
+            A2.Background = null;
+            A3.Background = null;
+            B1.Background = null;
+            B2.Background = null;
+            B3.Background = null;
+            C1.Background = null;
+            C2.Background = null;
+            C3.Background = null;
+        }
+    }
+}
---
+++ Model/Board.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_tac_toe_game.Model
+{
+    /// <summary>
+    /// Core class of the game
+    /// </summary>
+    public class Board
+    {
+        /// <summary>
+        /// X turn -> true  O turn -> false
+        /// (O) (O) (X)
+        /// (_) (X) (_)
+        /// (O) (X) (_)
+        /// 
+        /// 
+        /// (2) (2) (1)
+        /// (_) (1) (_)
+        /// (2) (1) (_)
+        /// A1 = 1 => крестик
+        /// A2 = 2 => нолик
+        /// 
+        /// </summary>
+        public bool Playerturn { get; set; }
+        public int A1 { get; set; }
+        public int A2 { get; set; }
+        public int A3 { get; set; }
+        public int B1 { get; set; }
+        public int B2 { get; set; }
+        public int B3 { get; set; }
+        public int C1 { get; set; }
+        public int C2 { get; set; }
+        public int C3 { get; set; }
+
+
+        public int X_count { get; set; }
+        public int O_count { get; set; }
+
+
+        /// <summary>
+        /// Проверка, победил ли кто-то из игроков
+        /// </summary>
+        /// <returns>
+        /// 1 -> X wins
+        /// 2 -> 0 wins
+        /// features: add win col/raw/diag inf (int ->[int,str,int])
+        /// </returns>
+        public int IsSomeoneWin()
+        {
+            if (this.IsSomeRowFilled() == 1 | this.IsSomeColomnFilled() == 1 | this.IsSomeDiagFilled() == 1)
+            {
+                this.RestartGame();
+                X_count += 1;
+                return 1; // крестики wins, игровое поле уже очищено, необходимо View окно привести к изначальному состоянию, с учетом изменения счёта
+            }
+            else if (this.IsSomeRowFilled() == 2 | this.IsSomeColomnFilled() == 2 | this.IsSomeDiagFilled() == 2)
+            {
+                this.RestartGame();
+                O_count -= 1;
+                return 2; // нолики wins
+            }
+            else
+            {
+                this.MakeTurn(); // смена хода
+                return 0; //играем дальше
+            }
+        }
+
+         
+
+        /// <summary>
+        /// Проверка, заполнил ли кто-то какой-либо ряд
+        /// </summary>
+        /// <returns></returns>
+        int IsSomeRowFilled()
+        {
+            if ((this.A1 == 1 && this.A2 == 1 && this.A3 == 1) |
+                (this.B1 == 1 && this.B2 == 1 && this.B3 == 1) |
+                (this.C1 == 1 && this.C2 == 1 && this.C3 == 1))
+                return 1;
+            else if ((this.A1 == 2 && this.A2 == 2 && this.A3 == 2) |
+                     (this.B1 == 2 && this.B2 == 2 && this.B3 == 2) |
+                     (this.C1 == 2 && this.C2 == 2 && this.C3 == 2))
+                return 2;
+            else
+                return 0;
+        }
+
+        /// <summary>
+        /// Проверка, заполнил ли кто-то какой-либо столбец
+        /// </summary>
+        /// <returns></returns>
+        int IsSomeColomnFilled()
+        {
+            if ((this.A1 == 1 && this.B1 == 1 && this.C1 == 1) |
+                (this.A2 == 1 && this.B2 == 1 && this.C2 == 1) |
+                (this.A3 == 1 && this.B3 == 1 && this.C3 == 1))
+                return 1;
+            else if ((this.A1 == 2 && this.B1 == 2 && this.C1 == 2) |
+                     (this.A2 == 2 && this.B2 == 2 && this.C2 == 2) |
+                     (this.A3 == 2 && this.B3 == 2 && this.C3 == 2))
+                return 2;
+            else
+                return 0;
+        }
+
+
+        /// <summary>
+        /// Проверка, заполнил ли кто-то какую-либо диагональ
+        /// </summary>
+        /// <returns></returns>
+        int IsSomeDiagFilled()
+        {
+            if ((this.A1 == 1 && this.B2 == 1 && this.C3 == 1) |
+                (this.A3 == 1 && this.B2 == 1 && this.C1 == 1))
+                return 1;
+            else if ((this.A1 == 2 && this.B2 == 2 && this.C3 == 2) |
+                     (this.A3 == 2 && this.B2 == 2 && this.C1 == 2))
+                return 2;
+            else
+                return 0;
+        }
+
+        void RestartGame()
+        {
+            Playerturn = true;
+            this.A1 = 0;
+            this.B1 = 0;
+            this.C1 = 0;
+            this.A2 = 0;
+            this.B2 = 0;
+            this.C2 = 0;
+            this.A3 = 0;
+            this.B3 = 0;
+            this.C3 = 0;
+        }
+
+        void MakeTurn()
+        {
+            if (this.Playerturn) this.Playerturn = false;
+            else this.Playerturn = true;
+        }
+
+        /// <summary>
+        /// Базовый конструктор, который заполняет игровое поле нулями
+        /// </summary>
+        public Board()
+        {
+            this.RestartGame();
+        }
+    }
+}
